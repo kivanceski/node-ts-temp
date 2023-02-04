652676a13f73b8c6aa4927026c899b895db7dfa8
Kivanc Eski <eskikivanc@gmail.com>
2023-02-04 23:57:36 +0300

    Add middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import { config } from 'dotenv'
 import morgan from 'morgan'
+import compression from 'compression'
+import helmet from 'helmet'
+import cors from 'cors'
 
 import routes from '@/routes'
 import { errorMiddleware } from './middleware/error'
@@ -9,9 +12,17 @@ config()
 
 const app = express()
 
+app.use(helmet())
+
+app.use(cors())
+
 app.use(morgan('dev'))
 
 app.use(express.json())
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use(compression())
 
 app.use('/api', routes)
 
